989e01c9bab07274c7acd8b0d4b5a81ab849a7aa
Adam Baratz <djz24@case.edu>
2014-06-26 15:30:21 -0400

    pem filepath error fixed

--- config/initializers/push.rb
+++ config/initializers/push.rb
@@ -1,5 +1,5 @@
 APNS.host = 'gateway.push.apple.com' 
 # gateway.sandbox.push.apple.com is default
 
-APNS.pem  = '/full-cert.pem'
+APNS.pem  = Rails.root + 'full-cert.pem'
 # this is the file you just created
